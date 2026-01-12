f30ebac1e6616e56ee6e316ef1f3d09c5bbc1f17
scriptMang <aperalta686@gmail.com>
2026-01-12 15:47:42 -0500

    Add instatus input field and grab its element

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let taskButton = document.getElementById("taskButton");
 let taskInput = document.getElementById("taskInput");
 let categoryInput = document.getElementById("categoryInput");
 let deadlineInput = document.getElementById("deadlineInput");
-
+let initialStatusInput = document.getElementById("initalStatusInput");
