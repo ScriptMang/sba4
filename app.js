3d5140633e7f1f6ae6192a3e80b736da047d0ecb
scriptMang <aperalta686@gmail.com>
2026-01-12 15:36:09 -0500

    Add vars for all input fields and button

---
+++ app.js
@@ -0,0 +1,5 @@
+let taskButton = document.getElementById("taskButton");
+let taskInput = document.getElementById("taskInput");
+let categoryInput = document.getElementById("categoryInput");
+let deadlineInput = document.getElementById("deadlineInput");
+
