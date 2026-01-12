61214cd440e3b2c5a5fcf1f388fdaf8d2c4f1734
scriptMang <aperalta686@gmail.com>
2026-01-12 16:04:11 -0500

    Add event listener to add task button

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let taskButton = document.getElementById("taskButton");
+let taskButton = document.getElementById("addTaskButton");
 let taskInput = document.getElementById("taskInput");
 let categoryInput = document.getElementById("categoryInput");
 let deadlineInput = document.getElementById("deadlineInput");
-let initialStatusInput = document.getElementById("initalStatusInput");
+let initialStatusInput = document.getElementById("initialStatusInput");
+
+taskButton.addEventListener("click", function(){
+    let inputFields = [taskInput, categoryInput, deadlineInput, initialStatusInput];
+    for (let inputField of inputFields) {
+        if (inputField.value === "") {
+            alert("No input field can be empty");
+            return;
+        }
+    }
+});
