c6bbda237468dad8678242c6b0419c20d08d9e1d
scriptMang <aperalta686@gmail.com>
2026-01-16 19:31:14 -0500

    Add tasks to task-list and prints the tasklist

--- app.js
+++ app.js
@@ -20,4 +20,9 @@ taskButton.addEventListener("click", function(){
         status:statusInput.value 
     };
     console.log(task);
+    taskList.push(task);
+    let taskCount = 0;
+    for (let task of taskList) {
+        console.log(`task${++taskCount}: \n` + Object.values(task));
+    }
 });
